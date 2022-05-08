02e8682a2d615f803797802a69a885c10032701b
yongchanson <syc9430@gmail.com>
2022-05-08 18:20:04 +0900

    6.1 분->시간, 시간->분

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
 
 function App() {
+  //useRecoilState는 atom, selector 둘다 사용가능
+  //[]의 첫번째 요소는 atom의 값 or selector의 get함수의 값
+  //[]의 두번째 요소는 atom을 수정하는 함수 or selector의 set property를 실행시키는 함수
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //+는 string->number 해줌
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
+      {/* onChange 함수가 있어야 수정가능함 */}
       <input
         value={minutes}
         onChange={onMinutesChange}
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,25 @@
 import { atom, selector } from "recoil";
 
+//state는 minutes 하나만 있는 상태, atom도 1개인 상태
+//hourSelect은 select를 활용해 state를 가져와서 수정한 output을 return중임
+
 export const minuteState = atom({
   key: "minutes",
   default: 0,
 });
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
   key: "hours",
+  //{get} : get이라는 함수를 불러 state를 가져옴
   get: ({ get }) => {
     const minutes = get(minuteState);
     return minutes / 60;
+  },
+  //set : state를 원하는 것으로 수정해줌
+  //=> minutes의 state(minuteState)를 newValue로 수정하기 위해 사용
+
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
